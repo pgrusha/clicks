02e1af998f4f058b2e6831cd559b0fde0f9d17db
pgrusha <pgrusha@sura.ru>
2020-07-25 16:06:36 +0300

    fix20200725_16

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import requests
 import os
 import sys
 import argparse
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 def create_parser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument ('url', nargs='?')
+    parser.add_argument ('url')
     return parser
 
 
@@ -19,13 +20,23 @@ def shorten_link(token, url):
     return response.json()['link']
 
 
-def count_clicks(token, link):
-    if link.startswith('https://'):
-        link = link[8:]
-    elif link.startswith('http://'):
-        link = link[7:]
+def is_short_link(token, url):
+    sep_url = urlparse(url)
     headers = {'Authorization':'Bearer {}'.format(token)}
-    service_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(link)
+    payload = {'bitlink_id': url[len(sep_url[0]) + 3:]}
+    service_url = 'https://api-ssl.bitly.com/v4/expand'
+    response = requests.post(service_url, headers=headers, json=payload)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        return False
+    return True
+
+
+def count_clicks(token, link):
+    sep_url = urlparse(link)
+    headers = {'Authorization':'Bearer {}'.format(token)}
+    service_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/{}/clicks/summary'.format(sep_url[1], sep_url[2])
     response = requests.get(service_url, headers=headers)
     response.raise_for_status()
     return response.json()['total_clicks']
@@ -36,13 +47,14 @@ if __name__ == '__main__':
     token = os.getenv("BITLY_TOKEN")
     parser = create_parser()
     cline_params = parser.parse_args()
-    if (cline_params.url.startswith('https://bit.ly/')):
+    if is_short_link(token, cline_params.url):
         try:
+            success = True
             clicks_count = count_clicks(token, cline_params.url)
         except requests.exceptions.HTTPError:
-            clicks_count = None
+            success = False
             print('Сервер вернул ошибку. Возможно, адрес неверный')
-        if clicks_count:
+        if success:
             print('Количество кликов', clicks_count)
     else:
         try:
